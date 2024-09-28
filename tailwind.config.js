d3a404f387b7f0a65ac9bc8a695186a3a0425ef3
Jeric June Logan <loganjeric25@gmail.com>
2024-09-28 22:50:01 +0800

    setup nativewind

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  darkMode: "class",
+  content: ["./app/**/*.{js,jsx,ts,tsx}", "./components/**/*.{js,jsx,ts,tsx}"],
+  presets: [require("nativewind/preset")],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
